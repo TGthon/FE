4e605ad614ae8089af8148f43c680940b0dc0010
ktjdus <3217sy@khu.ac.kr>
2025-08-14 15:53:10 +0900

    뒤로 가기 아이콘 변경

--- app/(event)/_layout.tsx
+++ app/(event)/_layout.tsx
@@ -1,7 +1,9 @@
-import { Stack } from 'expo-router';
-import { Platform } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+import { Platform, Pressable, Image } from 'react-native';
 
 export default function EventStackLayout() {
+    const router = useRouter();
+
   return (
     <Stack
       screenOptions={{
@@ -9,6 +11,18 @@ export default function EventStackLayout() {
         headerBackTitle: '뒤로',
         animation: Platform.select({ ios: 'slide_from_right', android: 'slide_from_right' }),
         contentStyle: { backgroundColor: '#fff' },
+        headerLeft: () => (
+            <Pressable
+              onPress={() => router.back()}
+              style={{ paddingHorizontal: 12, marginLeft: 8,}}
+            >
+                <Image
+                    source={require('../../assets/images/arrow-left.png')}
+                    style={{ width: 12, height: 24, resizeMode: 'contain' }}
+                    resizeMode="contain"
+                />
+            </Pressable>
+        ),
       }}
     ></Stack>
   );
